d5ff6159e4f0918e0a4f62690d8b590d57cd33d8
Dong-Chen-1031 <90507781+Dong-Chen-1031@users.noreply.github.com>
2021-12-18 20:22:48 +0800

    更新 main.ts, main.blocks

--- main.ts
+++ main.ts
@@ -34,55 +34,57 @@ _ = true
 bird.set(LedSpriteProperty.Blink, 500)
 game.setScore(0)
 basic.forever(function () {
-    if (_) {
-        vu6 = Math.round(input.rotation(Rotation.Pitch) / 15)
-        for (let index = 0; index <= Math.abs(vu6) - 1; index++) {
-            if (vu6 > 0) {
-                bird.change(LedSpriteProperty.Y, 1)
-            } else if (vu6 < 0) {
-                bird.change(LedSpriteProperty.Y, -1)
+    if (!(game.isPaused())) {
+        if (_) {
+            vu6 = Math.round(input.rotation(Rotation.Pitch) / 15)
+            for (let index = 0; index <= Math.abs(vu6) - 1; index++) {
+                if (vu6 > 0) {
+                    bird.change(LedSpriteProperty.Y, 1)
+                } else if (vu6 < 0) {
+                    bird.change(LedSpriteProperty.Y, -1)
+                }
             }
         }
-    }
-    while (obstacles.length > 0 && obstacles[0].get(LedSpriteProperty.X) == 0) {
-        obstacles.removeAt(0).delete()
-        zp += 1
-        if (zp % 4 == 0) {
-            music.playTone(659, music.beat(BeatFraction.Quarter))
-            music.playTone(988, music.beat(BeatFraction.Quarter))
-        }
-    }
-    for (let obstacle2 of obstacles) {
-        obstacle2.change(LedSpriteProperty.X, -1)
-    }
-    if (ticks % 3 == 0) {
-        emptyObstacleY = randint(0, 4)
-        for (let index2 = 0; index2 <= 4; index2++) {
-            if (index2 != emptyObstacleY) {
-                obstacles.push(game.createSprite(4, index2))
+        while (obstacles.length > 0 && obstacles[0].get(LedSpriteProperty.X) == 0) {
+            obstacles.removeAt(0).delete()
+            zp += 1
+            if (zp % 4 == 0) {
+                music.playTone(659, music.beat(BeatFraction.Quarter))
+                music.playTone(988, music.beat(BeatFraction.Quarter))
             }
         }
-    }
-    for (let obstacle3 of obstacles) {
-        if (obstacle3.get(LedSpriteProperty.X) == bird.get(LedSpriteProperty.X) && obstacle3.get(LedSpriteProperty.Y) == bird.get(LedSpriteProperty.Y)) {
-            music.playTone(392, music.beat(BeatFraction.Quarter))
-            music.playTone(349, music.beat(BeatFraction.Half))
-            music.playTone(330, music.beat(BeatFraction.Whole))
-            game.setScore(zp)
-            game.gameOver()
+        for (let obstacle2 of obstacles) {
+            obstacle2.change(LedSpriteProperty.X, -1)
         }
-    }
-    ticks += 1
-    time = 1000 - ticks * 12
-    if (time > 0) {
-        basic.pause(time)
-    } else {
-        basic.pause(0)
-    }
-    g03 = 500 - ticks * 10
-    if (g03 > 1) {
-        bird.set(LedSpriteProperty.Blink, g03)
-    } else {
-        bird.set(LedSpriteProperty.Blink, 1)
+        if (ticks % 3 == 0) {
+            emptyObstacleY = randint(0, 4)
+            for (let index2 = 0; index2 <= 4; index2++) {
+                if (index2 != emptyObstacleY) {
+                    obstacles.push(game.createSprite(4, index2))
+                }
+            }
+        }
+        for (let obstacle3 of obstacles) {
+            if (obstacle3.get(LedSpriteProperty.X) == bird.get(LedSpriteProperty.X) && obstacle3.get(LedSpriteProperty.Y) == bird.get(LedSpriteProperty.Y)) {
+                music.playTone(392, music.beat(BeatFraction.Quarter))
+                music.playTone(349, music.beat(BeatFraction.Half))
+                music.playTone(330, music.beat(BeatFraction.Whole))
+                game.setScore(zp)
+                game.gameOver()
+            }
+        }
+        ticks += 1
+        time = 1000 - ticks * 12
+        if (time > 0) {
+            basic.pause(time)
+        } else {
+            basic.pause(0)
+        }
+        g03 = 500 - ticks * 10
+        if (g03 > 1) {
+            bird.set(LedSpriteProperty.Blink, g03)
+        } else {
+            bird.set(LedSpriteProperty.Blink, 1)
+        }
     }
 })
